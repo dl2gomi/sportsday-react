59cb8695328ddf4b54463cbb07e6a740b035690b
Denis Lee <dl2gomi@gmail.com>
2024-08-01 05:55:01 -0500

    Skeleton loading implemented

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { MainLayout } from '../layouts';
 import { Grid, Box } from '@mui/material';
 import { useFetch, useLocalStorage } from '../hooks';
-import { EventCard } from '../components';
+import { EventCard, EventCardSkeleton } from '../components';
 import { useSnackbar } from 'notistack';
 
 const EventsPage = ({}) => {
@@ -35,6 +35,7 @@ const EventsPage = ({}) => {
     return start1 < end2 && start2 < end1;
   };
 
+  // check time overlap
   const checkOverlap = (item) => {
     const selectedEvents =
       (data &&
@@ -48,6 +49,7 @@ const EventsPage = ({}) => {
     );
   };
 
+  // check if max selection number reached
   useEffect(() => {
     selected.length >= 3 &&
       enqueueSnackbar({
@@ -57,6 +59,16 @@ const EventsPage = ({}) => {
       });
   }, [selected]);
 
+  // check if error occured during url fetch
+  useEffect(() => {
+    error &&
+      enqueueSnackbar({
+        variant: 'error',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' },
+        message: `An error occured during API fetch. ${error.message}`,
+      });
+  }, [error]);
+
   return (
     <MainLayout>
       <Grid container sx={{ flexGrow: 1 }}>
@@ -64,29 +76,46 @@ const EventsPage = ({}) => {
           <Box sx={{ mx: 2, my: 0 }} component="h3">
             All Events
           </Box>
-          <Grid container sx={{ flexGrow: 1 }}>
-            {data &&
-              Array.isArray(data) &&
-              data.map((item, key) => (
-                <Grid item key={key} xl={3} lg={4} md={6} xs={12} sx={{ p: 1 }}>
-                  <EventCard
-                    name={item.event_name}
-                    category={item.event_category}
-                    id={item.id}
-                    start={new Date(item.start_time)}
-                    end={new Date(item.end_time)}
-                    sx={{ width: 100 }}
-                    selected={selected && selected.includes(item.id)}
-                    overlapped={checkOverlap(item)}
-                    limited={
-                      selected.length >= 3 && !selected.includes(item.id)
-                    }
-                    select={selectEvent}
-                    remove={removeEvent}
-                  />
-                </Grid>
+          {loading && (
+            <Grid container sx={{ flexGrow: 1, ml: 1 }} width="100%">
+              {Array.from({ length: 6 }).map((_, index) => (
+                <EventCardSkeleton key={index}></EventCardSkeleton>
               ))}
-          </Grid>
+            </Grid>
+          )}
+          {!loading && (
+            <Grid container sx={{ flexGrow: 1 }}>
+              {data &&
+                Array.isArray(data) &&
+                data.map((item, key) => (
+                  <Grid
+                    item
+                    key={key}
+                    xl={3}
+                    lg={4}
+                    md={6}
+                    xs={12}
+                    sx={{ p: 1 }}
+                  >
+                    <EventCard
+                      name={item.event_name}
+                      category={item.event_category}
+                      id={item.id}
+                      start={new Date(item.start_time)}
+                      end={new Date(item.end_time)}
+                      sx={{ width: 100 }}
+                      selected={selected && selected.includes(item.id)}
+                      overlapped={checkOverlap(item)}
+                      limited={
+                        selected.length >= 3 && !selected.includes(item.id)
+                      }
+                      select={selectEvent}
+                      remove={removeEvent}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
+          )}
         </Grid>
         <Grid
           item
@@ -97,13 +126,22 @@ const EventsPage = ({}) => {
         >
           <Box sx={{ mx: 2, my: 0 }} component="h3">
             Selected Events
+          </Box>
+          {loading && (
+            <Grid container sx={{ flexGrow: 1, ml: 1 }} width="100%">
+              {Array.from({ length: 2 }).map((_, index) => (
+                <EventCardSkeleton key={index}></EventCardSkeleton>
+              ))}
+            </Grid>
+          )}
+          {!loading && (
             <Grid container sx={{ flexGrow: 1 }}>
               {data &&
                 Array.isArray(data) &&
                 data.map(
                   (item, key) =>
                     selected.includes(item.id) && (
-                      <Grid item key={key} xs={12} sx={{ p: 1 }}>
+                      <Grid item key={key} xs={12} xl={6} sx={{ p: 1 }}>
                         <EventCard
                           name={item.event_name}
                           category={item.event_category}
@@ -123,7 +161,7 @@ const EventsPage = ({}) => {
                     )
                 )}
             </Grid>
-          </Box>
+          )}
         </Grid>
       </Grid>
     </MainLayout>
