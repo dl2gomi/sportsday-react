d260bda7a8e081b59194c3fdc5fa2f5f964fb91a
Denis Lee <dl2gomi@gmail.com>
2024-07-31 22:32:35 -0500

    Header and Hook completed

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Card } from '@mui/material';
+
+const EventCard = ({}) => (
+  <>
+    <Card></Card>
+  </>
+);
+
+export default EventCard;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as HomePage } from './HomePage';
+export { default as EventsPage } from './EventsPage';
+export { default as NotFound } from './NotFound';
